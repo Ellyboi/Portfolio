a396bb901ec82d942a73d524619e581e9d821c24
ELIJAH <vitoesitemitope@gmail.com>
2023-01-04 15:15:30 +0000

    Update js file and linters for js according to the code reviewer discretion

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ document.querySelectorAll('.nave-link').forEach((n) => n.addEventListener('click
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
 }));
+
+function closeNav() {
+  navMenu.classList.toggle('invisible');
+  hamburger.classList.toggle("active");
+}
+
+navMenu.addEventListener('click', closeNav);
