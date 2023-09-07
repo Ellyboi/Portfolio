a14966ddcbe3d78b401102e99435ebe4af84bc5b
Vitoesi Elijah <vitoesitemitope@gmail.com>
2023-09-07 02:51:15 +0100

    update the project

---
+++ data/projects.js
@@ -0,0 +1,57 @@
+const projects = [
+  {
+    title: "Vecs-School",
+    description: "The online course reservation and school booking platform is a powerful system that simplifies the process of booking courses for users. It offers a user-friendly interface where individuals can specify their city of interest and preferred dates for course bookings. The platform is built on a robust backend server that communicates seamlessly with frontend applications, ensuring a smooth and efficient user experience.",
+    imageUrl: "./images/project_images/bookme.png",
+    technologies: ["Ruby on Rails", "React", "Redux", "JavaScript", "Rspec", "Bootstrap", "PostgreSQL"],
+    liveVersion: "https://vecs-school.onrender.com",
+    source: "https://github.com/Ellyboi/vecs-school-frontend",
+  },
+
+  {
+    title: "Math Magicians",
+    description: "Math Magicians is an engaging web application project that enables users to perform basic calculations while injecting a touch of humor into their experience. This interactive platform is constructed using the React framework, offering a seamless and user-friendly interface for mathematical operations combined with random, amusing quotes to keep users entertained and engaged.",
+    imageUrl: "./images/project_images/calculator.png",
+    technologies: ["MEALDB API", "JavaScript", "Webpack", "CSS", "HTML", "React", "ES6 modules"],
+    liveVersion: "https://math-magicians-wpnn.onrender.com/Home",
+    source: "https://github.com/Ellyboi/Math-Magicians",
+  },
+
+  {
+    title: "Country Details App",
+    description: "The 'Country Details App' is a web application that empowers users to filter and access comprehensive information about countries, including the count of their borders. In a nutshell, its main functionalities encompass showcasing a list of countries and presenting in-depth details about individual countries.",
+    imageUrl: "./images/project_images/reactcapstone.png",
+    technologies: ["React", "Redux", "Jest", "JavaScript", "CSS", "Html"],
+    liveVersion: "https://metricwebapp-h041.onrender.com/",
+    source: "https://github.com/Ellyboi/react-capstone-project",
+  },
+
+  {
+    title: "Leaderboard",
+    description: "'Leaderboard' is a dynamic project designed to facilitate user engagement by enabling them to submit their scores and view them on the games leaderboard. Crafted using a combination of essential web technologies, including HTML, CSS, JavaScript, and API integration, this project harnesses the power of Webpack for seamless bundling and deployment. Furthermore, it adheres to efficient version control practices through Gitflow, ensuring collaborative development and streamlined code management.",
+    imageUrl: "./images/project_images/leaderboard.png",
+    technologies: ["JavaScript", "Webpack", "CSS", "Html", "ES6 modules"],
+    liveVersion: "https://leaderboard-wp2o.onrender.com/",
+    source: "https://github.com/Ellyboi/leaderboard",
+  },
+
+  {
+    title: "Bookstore",
+    description: "This project is a feature-rich web application that provides users with the ability to seamlessly manage their book collection. Developed using the React framework and enhanced with Redux as the state management tool, it offers a comprehensive set of functionalities for adding, displaying, and removing selected books from the users collection. This ensures a smooth and efficient experience for users looking to organize and interact with their favorite books within the application.",
+    imageUrl: "./images/project_images/bookstore.png",
+    technologies: ["Webpack", "JavaScript", "ES Modules", "CSS", "HTML"],
+    liveVersion: "https://bookstore-3fl6.onrender.com/",
+    source: "https://github.com/Ellyboi/bookstore",
+  },
+
+  {
+    title: "Action Comics",
+    description: "This is an interactive movie platform designed to provide users with a curated selection of films sourced from an external API. The platform not only offers the opportunity to explore and watch these movies but also encourages user engagement through commenting and liking features",
+    imageUrl: "./images/project_images/bookme2.png",
+    technologies: ["React", "JavaScript", "HTML", "CSS", "ES6 modules", "API"],
+    liveVersion: "https://actionmovies.onrender.com/",
+    source: "https://github.com/Ellyboi/Javascript-capstone-Project",
+  },
+];
+
+export default projects;
